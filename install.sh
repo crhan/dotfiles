afb829e0ee852b14bf99e2cce3bb4cd80d8d0655
墨泪 <ruohan.chen@antgroup.com>
2022-04-13 23:55:50 +0000

    force set dotfiles

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ FOLDER=$(cd `dirname $0` && pwd)
 
 for f in `ls -a $FOLDER/links`
 do
-	if [ ! -e $FOLDER/../$f ]
-	then
+	# if [ ! -e $FOLDER/../$f ]
+	# then
 		echo Linking $f
 		ln -sf `basename $FOLDER`/links/$f `dirname $FOLDER`/$f
-	fi
+	# fi
 done
 
 echo REMOTE_CONTAINERS: $REMOTE_CONTAINERS
